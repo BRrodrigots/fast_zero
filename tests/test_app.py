b61c74a0158f351bb04df14473364a00b4e80045
BRrodrigots <rodrigo98ts@gmail.com>
2025-03-10 14:51:15 -0300

    test:

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,14 +1,9 @@
 from http import HTTPStatus
 
-from fastapi.testclient import TestClient
-
-from fast_zero.app import app
-
 
 def test_root_deve_retornar_ok_e_ola_mundo(client):
-    client = TestClient(app)  # Arrange
-
     response = client.get('/')  # Act
 
     assert response.status_code == HTTPStatus.OK  # Assert
+    
     assert response.json() == {'message': 'Olá Mundo!'}  # Assert
