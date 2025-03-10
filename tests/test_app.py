5edff97b4a96d196f5fe30de8e99185b80094017
BRrodrigots <rodrigo98ts@gmail.com>
2025-03-10 14:52:30 -0300

    test2:

--- tests/test_app.py
+++ tests/test_app.py
@@ -5,5 +5,4 @@ def test_root_deve_retornar_ok_e_ola_mundo(client):
     response = client.get('/')  # Act
 
     assert response.status_code == HTTPStatus.OK  # Assert
-    
     assert response.json() == {'message': 'Olá Mundo!'}  # Assert
